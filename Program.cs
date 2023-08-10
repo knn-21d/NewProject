7602ba86665fb9a56de55bea80fe8f3929452aa6
knn-21d <knn21d>
2023-08-11 00:44:55 +0300

    email confirmation made unnecessary

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace NewProject
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.ConfigureApplicationCookie(options =>
             {
